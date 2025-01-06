53ee83f2756275af33bbf821ddf19a3d37a8e4cc
Dan Moinescu <dan@moinescu.net>
2025-01-06 14:34:10 +0200

    Functional main program
    Compute the median of 2 vectors passed as arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,60 @@
 #include "median2.hpp"
 
+#include <algorithm>
 #include <iostream>
-using std::cout;
+#include <sstream>
 
-int main()
+
+static void usage(const char *me)
 {
+    std::cerr << "Usage: " << me << " a1[,a2,a3...] b1[,b2,b3...]\n";
+}
+
+
+static std::vector<int> parseNumbers(const char *allNumbers)
+{
+    std::vector<int> retval;
+    std::stringstream ss(allNumbers);
+    std::string numberStr;
+    while(std::getline(ss, numberStr, ','))
+    {
+        char *end;
+        int number = (int) strtol(numberStr.c_str(), &end, 10);
+        if(*end != 0)
+        {
+            throw std::invalid_argument(std::string("Invalid character: ") + *end);
+        }
+        retval.push_back(number);
+    }
+    return retval;
+}
+
+
+int main(int argc, const char *argv[])
+{
+    if(argc != 3)
+    {
+        usage(argv[0]);
+        return 1;
+    }
+
+    std::vector<int> v1,v2;
+    try
+    {
+        v1 = parseNumbers(argv[1]);
+        v2 = parseNumbers(argv[2]);
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        usage(argv[0]);
+        return 1;
+    }
+    std::sort(v1.begin(), v1.end());
+    std::sort(v2.begin(), v2.end());
+
     Solution s;
-    double median = s.findMedianSortedArrays(
-            {10, 20, 30, 40, 50},
-            {2, 4, 7, 11, 12, 35, 36}
-            );
-    std::cout << median << "\n";
+    double median = s.findMedianSortedArrays(v1, v2);
+    std::cout << median << '\n';
     return 0;
 }
